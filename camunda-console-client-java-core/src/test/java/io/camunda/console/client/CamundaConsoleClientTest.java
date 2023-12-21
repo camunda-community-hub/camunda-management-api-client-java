abf2cc5ca6f77a7b9c46efe58530785b2492c8e7
Jonathan Lukas <jonathan.lukas@camunda.com>
2023-12-21 09:43:19 +0100

    formatter

--- camunda-console-client-java-core/src/test/java/io/camunda/console/client/CamundaConsoleClientTest.java
+++ camunda-console-client-java-core/src/test/java/io/camunda/console/client/CamundaConsoleClientTest.java
@@ -1,15 +1,9 @@
 package io.camunda.console.client;
 
-import io.camunda.console.client.api.CamundaConsoleClient;
-import io.camunda.console.client.model.Cluster;
-import io.camunda.console.client.properties.CamundaConsoleClientProperties;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CamundaConsoleClientTest {
   // TODO create a wiremock test here
   @Test
-  void shouldWork() {
-
-  }
+  void shouldWork() {}
 }
